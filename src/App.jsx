5e084b8220f316145f0da5e9f67a83be3575121c
Monika <laiskutis.monikai@gmail.com>
2024-08-09 12:29:09 +0300

    upd

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import { Score } from "./components/score/Score"
 
-
 function App() {
-
   return (
     <>
     <h1>Scoreboard</h1>
     <div className="scoreBoard">
         <Score/>
-        <div className="results"></div>
+        <div className="statistic"></div>
     </div>
     </>
   );
